95dd44543a81ad4545fc7c5e90228e3c37008378
zrunker <zrunker@yahoo.com>
2020-07-02 20:46:24 +0800

    init Commit

--- app/src/main/java/cc/ibooker/android/zlamemp3/MainActivity.java
+++ app/src/main/java/cc/ibooker/android/zlamemp3/MainActivity.java
@@ -3,6 +3,9 @@ package cc.ibooker.android.zlamemp3;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import cc.ibooker.android.zlamemp3lib.Mp3Converter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.tv);
+        tv.setText(Mp3Converter.getLameVersion());
     }
 }
